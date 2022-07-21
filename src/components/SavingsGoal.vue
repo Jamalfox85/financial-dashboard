703d24a2107f9c3ed0f8952500fdc81e069e6926
Jamal Fox <jamalfox@Jamals-MacBook-Air.local>
2022-07-21 01:05:26 -0400

    init commit

---
+++ src/components/SavingsGoal.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+	<div class="greetings">
+		<h1 class="green">Testing</h1>
+	</div>
+</template>
+
+<style scoped></style>
---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="dashboard-wrapper">
+		<h1 class="font-bold underline">Dashboard</h1>
+		<savings-goal />
+	</div>
+</template>
+
+<script>
+import SavingsGoal from "../components/SavingsGoal.vue";
+
+export default {
+	components: { SavingsGoal },
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard-wrapper {
+	height: 100vh;
+	width: 100%;
+}
+</style>
