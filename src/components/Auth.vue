0a85d59237dc1ec86df1ed679426e4e4bcdd3f3a
Jamal Fox <jamalfox@Jamals-MacBook-Air.local>
2022-12-06 01:24:07 -0500

    restyled login page

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -2,159 +2,185 @@
 import { ref } from "vue";
 import { supabase } from "../supabase";
 
+const makeToast = (variant = null) => {
+  this.$bvToast.toast("Toast body content", {
+    title: `Variant ${variant || "default"}`,
+    variant: variant,
+    solid: true,
+  });
+};
+
 const loading = ref(false);
+const firstName = ref("");
+const lastName = ref("");
 const email = ref("");
 const password = ref("");
+const confirmPassword = ref("");
+const formErrorMessage = ref("");
 
 /* logInType 0 = Sign Up, LogInType = 1 = Log In */
-let logInType = ref(1);
+let logInType = ref(0);
 
 const switchLogInType = () => {
+  formErrorMessage.value = "";
   logInType.value = logInType.value == 0 ? 1 : 0;
 };
 
 const handleLogin = async () => {
-  console.log("LOG IN");
   try {
     loading.value = true;
     const { data, error } = await supabase.auth.signInWithPassword({
       email: email.value,
       password: password.value,
     });
+    if (error) throw error;
   } catch (error) {
-    if (error instanceof Error) {
-      alert(error.message);
-    }
+    formErrorMessage.value = "Password is incorrect!";
   } finally {
     loading.value = false;
   }
 };
 
 const handleSignup = async () => {
-  console.log("SIGN UP");
-  try {
-    // Use the Supabase provided method to handle the signup
-    const { error } = await supabase.auth.signUp({
-      email: email.value,
-      password: password.value,
-    });
-    if (error) throw error;
-  } catch (error) {
-    alert(error.error_description || error.message);
+  if (password.value !== confirmPassword.value) {
+    formErrorMessage.value = "Passwords do not match!";
+    return;
+  } else {
+    try {
+      // Use the Supabase provided method to handle the signup
+      const { error } = await supabase.auth.signUp({
+        email: email.value,
+        password: password.value,
+      });
+      if (error) throw error;
+      if (!error) {
+        firstName.value = "";
+        lastName.value = "";
+        email.value = "";
+        password.value = "";
+        confirmPassword.value = "";
+      }
+    } catch (error) {
+      alert(error.error_description || error.message);
+    }
+  }
+};
+
+const handleFormSubmit = () => {
+  formErrorMessage.value = "";
+  if (logInType.value === 0) {
+    handleSignup();
+  } else {
+    handleLogin();
   }
 };
 </script>
 
 <template>
-  <!-- <form class="row flex-center flex" @submit.prevent="handleLogin">
-    <div class="col-6 form-widget">
-      <h1 class="header">Supabase + Vue 3</h1>
-      <p class="description">Sign in via magic link with your email below</p>
-      <div>
-        <input
-          class="inputField"
-          type="email"
-          placeholder="Your email"
-          v-model="email"
-        />
-      </div>
-      <div>
-        <input
-          type="submit"
-          class="button block"
-          :value="loading ? 'Loading' : 'Send magic link'"
-          :disabled="loading"
-        />
-        <input
-          type="submit"
-          class="button block"
-          :value="'Sign Up'"
-          :disabled="loading"
-          @click="handleSignup"
-        />
-      </div>
-    </div>
-  </form> -->
   <div class="auth_wrapper">
-    <!-- Sign Up -->
-    <div class="auth_main_content" v-if="logInType === 0">
-      <div class="auth_type_text">
-        <h2>Sign Up</h2>
-        <h3>
+    <div class="side-content">
+      <p class="logo">Tauro</p>
+      <div class="main-text">
+        <h1 class="main-auth-text">Welcome to Tauro Finance.</h1>
+        <h2 class="secondary-auth-text">
+          Its time to take back control of your financial future.
+        </h2>
+      </div>
+      <img
+        class="auth-login-image"
+        src="../assets/images/undraw_credit_card_re_blml.png"
+      />
+    </div>
+    <div class="form-content">
+      <!-- Sign Up Header -->
+      <div class="form-header" v-if="logInType === 0">
+        <h1 class="form-header-text">Sign Up</h1>
+        <h2 class="form-subheader-text">
           Already have an account?
           <button @click="switchLogInType" class="switch-login-type">
-            Log In Here</button
-          >.
-        </h3>
+            Log In Here!
+          </button>
+        </h2>
       </div>
-      <form class="form-content" @submit.prevent="handleSignup">
-        <label class="form-label">Email</label>
-        <input
-          class="form-input"
-          type="email"
-          placeholder="Your email"
-          v-model="email"
-        />
-        <label class="form-label">Password</label>
-        <input
-          class="form-input"
-          type="text"
-          placeholder="password123"
-          v-model="password"
-        />
-        <button class="auth-bttn" type="submit">Submit</button>
-      </form>
-      <!-- <div class="alternative-login-options">
-        <img class="alternative-login-img" src="../assets/icons/github.png" />
-        <img class="alternative-login-img" src="../assets/icons/apple.png" />
-        <img class="alternative-login-img" src="../assets/icons/google.png" />
-      </div> -->
-      <img
-        class="auth-login-image"
-        src="../assets/images/undraw_credit_card_re_blml.png"
-      />
-    </div>
-
-    <!-- Log In -->
-    <div class="auth_main_content" v-if="logInType === 1">
-      <div class="auth_type_text">
-        <h2>Log In</h2>
-        <h3>
-          New to Tauro Finance?
+      <!-- Log In Header -->
+      <div class="form-header" v-if="logInType === 1">
+        <h1 class="form-header-text">Log In</h1>
+        <h2 class="form-subheader-text">
+          Don't have an account?
           <button @click="switchLogInType" class="switch-login-type">
-            Create an Account Here</button
-          >.
-        </h3>
+            Sign Up Here!
+          </button>
+        </h2>
       </div>
-      <form class="form-content" @submit.prevent="handleLogin">
-        <label class="form-label">Email</label>
-        <input
-          class="form-input"
-          type="email"
-          placeholder="Your email"
-          v-model="email"
-        />
-        <label class="form-label">Password</label>
-        <input
-          class="form-input"
-          type="text"
-          placeholder="password123"
-          v-model="password"
-        />
-        <button class="auth-bttn" type="submit">Submit</button>
-      </form>
-      <!-- <div class="alternative-login-options">
-        <img class="alternative-login-img" src="../assets/icons/github.png" />
-        <img class="alternative-login-img" src="../assets/icons/apple.png" />
-        <img class="alternative-login-img" src="../assets/icons/google.png" />
-      </div> -->
-      <img
-        class="auth-login-image"
-        src="../assets/images/undraw_credit_card_re_blml.png"
-      />
-    </div>
-    <div class="auth_side_content">
-      <h1 class="main-auth-text">Welcome to Tauro Finance</h1>
+      <div class="form-wrapper">
+        <p class="form-error-message">{{ formErrorMessage }}</p>
+        <form class="form-main" @submit.prevent="handleFormSubmit">
+          <div class="user-name" v-if="logInType === 0">
+            <label for="firstName" class="input-label">
+              First Name
+              <input
+                class="form-input"
+                type="text"
+                v-model="firstName"
+                id="firstName"
+                name="firstName"
+                :required="logInType === 0"
+              />
+            </label>
+            <label for="lastName" class="input-label">
+              Last Name
+              <input
+                class="form-input"
+                type="text"
+                v-model="lastName"
+                id="lastName"
+                name="lastName"
+                :required="logInType === 0"
+              />
+            </label>
+          </div>
+          <label for="email" class="input-label">
+            Email
+            <input
+              class="form-input"
+              type="email"
+              v-model="email"
+              id="email"
+              name="email"
+              required
+            />
+          </label>
+          <label for="password" class="input-label">
+            Password
+            <input
+              class="form-input"
+              type="password"
+              v-model="password"
+              id="password"
+              name="password"
+              required
+            />
+          </label>
+          <label
+            for="confirmPassword"
+            class="input-label"
+            v-if="logInType === 0"
+          >
+            Confirm Password
+            <input
+              class="form-input"
+              type="password"
+              v-model="confirmPassword"
+              id="confirmPassword"
+              name="confirmPassword"
+              :required="logInType === 0"
+            />
+          </label>
+          <button class="auth-submit-bttn" type="submit">
+            {{ logInType === 0 ? "Create an Account" : "Log In" }}
+          </button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -162,75 +188,105 @@ const handleSignup = async () => {
 <style lang="scss" scoped>
 .auth_wrapper {
   display: flex;
-  background-color: #342e37;
-  color: #fff;
   height: 100vh;
   width: 100%;
-  padding: 2em 0;
-  .auth_main_content {
+  padding: 2em;
+  .side-content {
+    border: solid 2px #a2d729;
+    background: rgb(162, 215, 41);
+    background: linear-gradient(
+      0deg,
+      rgba(162, 215, 41, 1) 0%,
+      rgba(70, 215, 32, 1) 70%
+    );
+    border-radius: 12px;
+    width: 450px;
+    padding: 2em;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    background-color: rgba(255, 255, 255, 0.1);
-    border: solid 2px #a2d729;
-    width: 70%;
-    border-radius: 0 24px 24px 0;
-    padding: 2em 10vw;
-    position: relative;
-    .auth_type_text {
-      margin-bottom: 2em;
-      h2 {
-        font-size: 36px;
+    justify-content: space-between;
+    .logo {
+      font-size: 24px;
+    }
+    .main-text {
+      .main-auth-text {
+        font-size: 48px;
+        margin-bottom: 12px;
+      }
+      .secondary-auth-text {
+        font-size: 24px;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+          Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+          sans-serif;
+        font-weight: lighter;
+      }
+    }
+  }
+  .form-content {
+    flex-grow: 1;
+    padding: 2em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .form-header {
+      margin-bottom: 4em;
+      .form-header-text {
+        font-size: 46px;
       }
       .switch-login-type {
         color: #a2d729;
+        transition: 0.05s ease-in;
+        position: absolute;
+        margin-left: 4px;
+        &:hover {
+          color: #46d729;
+          font-size: 1.1em;
+        }
       }
     }
-    .form-content {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      font-size: 24px;
-
-      .form-label {
-        letter-spacing: 4px;
-        margin-bottom: 1em;
+    .form-wrapper {
+      flex-grow: 1;
+      .form-error-message {
+        color: #d7c929;
       }
-      .form-input {
-        border-radius: 24px;
-        border: solid 1px #a2d729;
-        background-color: rgba(255, 255, 255, 0.1);
-        width: 400px;
-        margin-bottom: 3em;
+      .form-main {
+        width: 500px;
+        margin-top: 1em;
+        .user-name {
+          display: flex;
+          justify-content: space-between;
+          .input-label input {
+            width: 100%;
+          }
+        }
+        .input-label {
+          color: #fff;
+          display: flex;
+          flex-direction: column;
+          margin: 0 1em 1em 0;
+          font-size: 20px;
+          input {
+            width: 100%;
+            margin-top: 8px;
+            border-radius: 8px;
+            background-color: transparent;
+            border: solid 1px #fff;
+            padding: 8px 24px;
+          }
+        }
       }
-      .auth-bttn {
-        background-color: #a2d729;
-        width: 150px;
-        border-radius: 24px;
+      .auth-submit-bttn {
+        margin-top: 2em;
+        padding: 0.5em 1em;
+        font-size: 24px;
+        border-radius: 8px;
+        background: rgb(162, 215, 41);
+        background: linear-gradient(
+          180deg,
+          rgba(162, 215, 41, 1) 0%,
+          rgba(70, 215, 32, 1) 70%
+        );
       }
-    }
-    .alternative-login-options {
-      display: flex;
-      margin-top: 6em;
-      .alternative-login-img {
-        margin-right: 2em;
-      }
-    }
-    .auth-login-image {
-      position: absolute;
-      bottom: 4em;
-      right: -7em;
-      transform: rotate(-15deg);
-      width: 50%;
-    }
-  }
-  .auth_side_content {
-    display: flex;
-    justify-content: center;
-    flex-grow: 1;
-    .main-auth-text {
-      font-size: 48px;
-      width: 350px;
     }
   }
 }
